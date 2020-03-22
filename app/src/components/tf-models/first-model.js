0f3196a647fc8b639d0e2b1fa0079a36fc3759cb
Scott Siegel <scottnsiegel@gmail.com>
2020-03-22 00:47:10 +0000

    modified packages to incorporate three.js and storybook. added a personal tips-to-remember.txt file for the tutorial

--- app/src/components/tf-models/first-model.js
+++ app/src/components/tf-models/first-model.js
@@ -1,21 +1,21 @@
 import React from "react"
 import * as tf from '@tensorflow/tfjs';
-export default function FirstModel(){
-// Define a model for linear regression.
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+export default function FirstModel() {
+  // Define a model for linear regression.
+  const model = tf.sequential();
+  model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
 
-// Prepare the model for training: Specify the loss and the optimizer.
-model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+  // Prepare the model for training: Specify the loss and the optimizer.
+  model.compile({ loss: 'meanSquaredError', optimizer: 'sgd' });
 
-// Generate some synthetic data for training.
-const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+  // Generate some synthetic data for training.
+  const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+  const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
 
-// Train the model using the data.
-model.fit(xs, ys).then(() => {
-  // Use the model to do inference on a data point the model hasn't seen before:
-  const str=model.predict(tf.tensor2d([5], [1, 1])).print();
-});
-return <div />
+  // Train the model using the data.
+  model.fit(xs, ys).then(() => {
+    // Use the model to do inference on a data point the model hasn't seen before:
+    const str = model.predict(tf.tensor2d([5], [1, 1])).print();
+  });
+  return <div />
 } 
